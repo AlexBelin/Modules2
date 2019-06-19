117f7ba847b2e0ad82a981f088b771b91d822eb1
AlexBelin <47112218+AlexBelin@users.noreply.github.com>
2019-06-19 15:56:06 +0200

    Add files via upload

--- Countdown/countdown.js
+++ Countdown/countdown.js
@@ -1,9 +1,41 @@
+function CountDownElement(ElemValue, ElemLegend, CountdownSys){
+    this.ElemValue = ElemValue;
+    this.ElemLegend = ElemLegend;
+    this.CountdownSys = CountdownSys;
+
+    this.MakeElement = function(){
+        this.Cell = document.createElement('div');
+        this.Cell.classList.add('countdown-cell');
+        this.Value = document.createElement('div');
+        this.Legend = document.createElement('div');
+        this.Legend.classList.add('cell-legend');
+        this.Legend.innerHTML = this.ElemLegend;
+        this.Cell.appendChild(this.Value);
+        this.Cell.appendChild(this.Legend);
+    };
+
+    this.UpdateElement = function(_Value){
+        this.ElemValue = _Value;
+        if(this.ElemValue < 10){
+            if((Number(this.ElemValue) == 0) && (this.CountdownSys.ElemsList[0] === this)){
+                this.Cell.remove();
+                this.CountdownSys.ElemsList.splice(0, 1);
+            }
+            else{
+                this.ElemValue = '0' + this.ElemValue;
+            }
+        }
+        this.Value.innerHTML = this.ElemValue;
+    };
+}
+
 function CountdownSys(ElemID, EndTime, TitleText, EndEventText){
     this.ElemID = ElemID;
     this.CountdownContainer = document.getElementById(this.ElemID);
     this.EndTime = EndTime;
     this.TitleText = TitleText;
     this.EndEventText = EndEventText;
+    this.ElemsList = [];
 
     this.ActivateCountdown = function(){
         this.MakeElements();
@@ -26,20 +58,25 @@ function CountdownSys(ElemID, EndTime, TitleText, EndEventText){
 
         this.RowCells = document.createElement('div');
         this.RowCells.classList.add('row-cells');
-        this.DaysCell = document.createElement('div');
-        this.DaysCell.classList.add('countdown-cell');
-        this.HoursCell = document.createElement('div');
-        this.HoursCell.classList.add('countdown-cell');
-        this.MinutesCell = document.createElement('div');
-        this.MinutesCell.classList.add('countdown-cell');
-        this.SecsCell = document.createElement('div');
-        this.SecsCell.classList.add('countdown-cell');
+        this.Days = new CountDownElement(0, 'jours', this);
+        this.Days.MakeElement();
+        this.ElemsList.push(this.Days);
+        this.Hours = new CountDownElement(0, 'heures', this);
+        this.Hours.MakeElement();
+        this.ElemsList.push(this.Hours);
+        this.Minutes = new CountDownElement(0, 'minutes', this);
+        this.Minutes.MakeElement();
+        this.ElemsList.push(this.Minutes);
+        this.Seconds = new CountDownElement(0, 'secondes', this);
+        this.Seconds.MakeElement();
+        this.ElemsList.push(this.Seconds);
 
+        this.RowCells.appendChild(this.Days.Cell);
+        this.RowCells.appendChild(this.Hours.Cell);
+        this.RowCells.appendChild(this.Minutes.Cell);
+        this.RowCells.appendChild(this.Seconds.Cell);
+        
         this.CountdownContainer.appendChild(this.Title);
-        this.RowCells.appendChild(this.DaysCell);
-        this.RowCells.appendChild(this.HoursCell);
-        this.RowCells.appendChild(this.MinutesCell);
-        this.RowCells.appendChild(this.SecsCell);
         _ContentInfos.appendChild(this.RowCells);
         _ContentInfos.appendChild(this.EndEvent);
         this.CountdownContainer.appendChild(_ContentInfos);
@@ -55,10 +92,10 @@ function CountdownSys(ElemID, EndTime, TitleText, EndEventText){
             var _hours = Math.floor((_Difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             var _minutes = Math.floor((_Difference % (1000 * 60 * 60)) / (1000 * 60));
             var _seconds = Math.floor((_Difference % (1000 * 60)) / 1000);
-            _this.DaysCell.innerHTML = _days;
-            _this.HoursCell.innerHTML = _hours;
-            _this.MinutesCell.innerHTML = _minutes;
-            _this.SecsCell.innerHTML = _seconds;
+            _this.Days.UpdateElement(_days);
+            _this.Hours.UpdateElement(_hours);
+            _this.Minutes.UpdateElement(_minutes);
+            _this.Seconds.UpdateElement(_seconds);
             if((_days <= 0) && (_hours <= 0) && (_minutes <= 0) && (_seconds < 1)){
                 clearInterval(_CountDown);
                 _this.EndEvent.classList.add('revealed');
